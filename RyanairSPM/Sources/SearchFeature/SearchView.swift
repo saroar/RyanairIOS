01ca82e1ad2024c88a4a307e616e9998b98166ae
saroar <saroar9@gmail.com>
2022-02-21 14:37:00 +0300

    add fullScreenCoverCompat to swiftui extension

--- RyanairSPM/Sources/SearchFeature/SearchView.swift
+++ RyanairSPM/Sources/SearchFeature/SearchView.swift
@@ -253,36 +253,3 @@ struct SearchView_Previews: PreviewProvider {
 }
 
 
-struct FullScreenCoverCompat<CoverContent: View>: ViewModifier {
-	@Binding var isPresented: Bool
-	let content: () -> CoverContent
-
-	func body(content: Content) -> some View {
-		GeometryReader { geo in
-			ZStack {
-				// this color makes sure that its enclosing ZStack
-				// (and the GeometryReader) fill the entire screen,
-				// allowing to know its full height
-				Color.clear
-				content
-				ZStack {
-					// the color is here for the cover to fill
-					// the entire screen regardless of its content
-					Color.white
-					self.content()
-				}
-				.offset(y: isPresented ? 0 : geo.size.height)
-				// feel free to play around with the animation speeds!
-				.animation(.spring())
-			}
-		}
-	}
-}
-
-extension View {
-	func fullScreenCoverCompat<Content: View>(isPresented: Binding<Bool>,
-											  content: @escaping () -> Content) -> some View {
-		self.modifier(FullScreenCoverCompat(isPresented: isPresented,
-											content: content))
-	}
-}
